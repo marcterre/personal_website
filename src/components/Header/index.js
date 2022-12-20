e9cb6761d20cecbbb58884e12f67ee353c20ee8c
Marc Terre <marc.terre@outlook.de>
2022-12-20 23:01:56 +0100

    add structure, header, navigation

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import Navgation from "../Navigation";
+
+export default function Header() {
+  return (
+    <header>
+      <title>This is my universe.</title>
+      <Navgation></Navgation>
+    </header>
+  );
+}
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,15 @@
+export default function Navgation() {
+  return (
+    <ul>
+      <li>
+        <button>About me</button>
+      </li>
+      <li>
+        <button>My Projects</button>
+      </li>
+      <li>
+        <button>Contact</button>
+      </li>
+    </ul>
+  );
+}
