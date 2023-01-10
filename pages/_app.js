6560d1e964f5962b07d6f74589f1278c750c94a0
Marc Terre <marc.terre@outlook.de>
2023-01-11 00:09:27 +0100

    add BurgerMenu

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import GlobalStyles from "../components/GlobalStyles";
 import Navigation from "./navigation";
-
+import "../styles/burger.css";
 export default function App({ Component, pageProps }) {
+  const HamburgerIcon = () => (
+    <div className="p-1/2">
+      <svg
+        className="w-8 h-8 text-gray-500"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </div>
+  );
+
   return (
     <>
       <GlobalStyles />
--- pages/navigation/index.js
+++ pages/navigation/index.js
@@ -1,20 +1,31 @@
 import Link from "next/link";
+import HamburgerMenu, { Links } from "../../components/HambugerMenu";
 
 export default function Navigation() {
   return (
-    <header>
-      <h1>Welcome to my universe </h1>
-      <ul>
-        <li>
-          <Link href="../about-me">About me</Link>
-        </li>
-        <li>
-          <Link href="../projects">My Projects</Link>
-        </li>
-        <li>
-          <Link href="../Contact">Contact</Link>
-        </li>
-      </ul>
-    </header>
+    <div className="max-w-full h-12 flex justify-start items-center bg-black mb-4 text-white rounded-md ">
+      <div className="flex md:hidden">
+        <HamburgerMenu />
+      </div>
+      <div className="hidden md:flex">
+        <Links />
+      </div>
+    </div>
   );
+  // return (
+  //   <header>
+  //     <h1>Welcome to my universe </h1>
+  //     <ul>
+  //       <li>
+  //         <Link href="../about-me">About me</Link>
+  //       </li>
+  //       <li>
+  //         <Link href="../projects">My Projects</Link>
+  //       </li>
+  //       <li>
+  //         <Link href="../Contact">Contact</Link>
+  //       </li>
+  //     </ul>
+  //   </header>
+  // );
 }
